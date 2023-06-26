c1c3c5bfcac9f650e4d47bb6ebe2abdce27f8d55
7digitec <7digitec@gmail.com>
2023-06-26 16:02:04 +0200

    Changed main

--- main.tf
+++ main.tf
@@ -8,7 +8,9 @@ resource "aws_instance" "instance" {
   key_name               = var.instance_key_name
   vpc_security_group_ids = [aws_security_group.SG1.id]
   subnet_id              = var.instance_subnet_id
-  count                  = var.instances_count
+  count                  = length(data.aws_availability_zones.available_zones.names)
+  availability_zone      = data.aws_availability_zones.available_zones.names[count.index]
+
   # root disk
   root_block_device {
     volume_size           = var.instance_root_volume_size
@@ -16,14 +18,13 @@ resource "aws_instance" "instance" {
     delete_on_termination = true
     encrypted             = true
   }
-  #availability_zone      = data.aws_availability_zones.available_zones.names[count.index]
-  
+
   tags = {
     Name        = "${var.app_environment}-instance-${count.index+1}"
     Environment = var.app_environment
-  #  Name       = "instance${instances_count.index+1}"
-  #  Env        = "production"
-  #  ServerType = "backend"
+    Name       = "instance_{instances_count.index+1}"
+    Env        = "production"
+    ServerType = "backend"
   }
 
 
@@ -31,52 +32,28 @@ resource "aws_instance" "instance" {
 
 }
 
-#resource "aws_instance" "second_instance" {
-#  ami                    = "ami-04e601abe3e1a910f"
-#  instance_type          = var.instance_type
-#  key_name               = var.key2
-#  vpc_security_group_ids = [aws_security_group.SG1.id]
-#  count                  = length(data.aws_availability_zones.available_zones.names)
-#  availability_zone      = data.aws_availability_zones.available_zones.names[count.index]
-#  availability_zone = data.aws_availability_zones.available_zones.names[1]
-
-
-#  tags = {
-#    Name       = "second_instance_name"
-#    Env        = "production"
-#    ServerType = "frontend"
-#  }
-
-#  depends_on = [aws_instance.first_instance]
-#  user_data  = templatefile("01-ssh-public-key-config.tpl", { key_owner = "digitec@HP" })
-#}
 
 resource "aws_ebs_volume" "ssdgp1" {
-  count                 = var.instance_create_volume == "true" ? 1 : 0
-#  availability_zone     = aws_instance.instance.availability_zone
+  count                 = length(data.aws_availability_zones.available_zones.names)
+  availability_zone     = data.aws_availability_zones.available_zones.names[count.index]
   size                  = var.instance_ebs_volume_size
   type                  = var.instance_ebs_volume_type
-  #availability_zone     = [aws_instance.instance.*.availability_zone[count.index]]
- availability_zone     = var.region
- # [aws_instance.instance.*.availability_zone[count.index]]
- 
+
 #  kms_key_id = "Optional"
   tags = {
     Name       = "ssdgp1"
     Env        = "production"
     Size       = "100 Gb"
   }
-#delete_on_termination = true
 }
 
 resource "aws_volume_attachment" "ssdgp1" {
-#  count             = length(data.aws_availability_zones.available_zones.names)
-#    availability_zone = data.aws_availability_zones.available_zones.names[count.index]
 
-  count                 = var.instance_create_volume == "true" ? 1 : 0
+  count             = length(data.aws_availability_zones.available_zones.names)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   device_name           = var.instance_device_name1
   volume_id             = aws_ebs_volume.ssdgp1.id
-  instance_id           = aws_instance.instance.id
+  instance_id           = aws_instance.instance[count.index].id
 
 #  device_name = "/dev/xvdf"
 #  instance_id = "aws_instance.instance.id"
@@ -91,7 +68,7 @@ resource "aws_security_group" "SG1" {
   name        = "SG1"
   description = "Open ports: 22, 80, 443, 3000"
   vpc_id      = aws_default_vpc.default_vpc.id
-  
+
   dynamic "ingress" {
     for_each = var.ingress_ports
     content {
